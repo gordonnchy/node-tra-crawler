e37a012683f48d7fbb6f41d59bc72d8bb5461cfc
gordonnchy <godsonnchy@gmail.com>
2022-11-11 23:13:48 +0300

    wip

--- app.js
+++ app.js
@@ -4,48 +4,54 @@ const app = express();
 const db = require("./database.js");
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.listen(port, "172.20.10.8");
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/receipt/:code/:time', (req, res) => {
-    try {
-        db.get('SELECT * FROM receipts WHERE receipt_code = ?', [req.params.code], (err, rows) => {
-            if (err) {
-                res.status(400).json({"error":err.message});
-                return;
-            }
-            res.json({
-                "message": "success",
-                "data": rows
-            });
-            return;
-        });
+app.get('/receipt/:code/:time', async (req, res) => {
+    const response = await run(req.params.code, req.params.time);
 
-        run(req.params.code, req.params.time)
-            .then((response) => {
-                console.log(response);
-                var data = response[0];
-                if (data) {
-                    db.run(`INSERT INTO receipts (receipt_code,company_name,po_box,mobile,tin,vrn,serial_no,uin,tax_office,customer_name,customer_id_type,customer_id, customer_mobile,receipt_no,z_number,receipt_date,receipt_time,total_excl_of_tax,total_tax,total_incl_of_tax) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
-                        [req.params.code,data.companyName, data.poBox, data.mobile, data.tin, data.vrn, data.serialNo, data.uin, data.taxOffice, data.customerName, data.customerIdType, data.customerId, data.customerMobile, data.receiptNo, data.zNumber, data.receiptDate, data.receiptTime, data.totalExclOfTax, data.totalTax, data.totalInclOfTax]
-                    );
-                    res.send(data);
-                    return;
-                }
-
-                res.send({status: true});
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    } catch {
-        res.json({});
+    if (response) {
+        res.send(response[0]);
+    } else {
+        console.log(response);
     }
+    // try {
+    //     // db.get('SELECT * FROM receipts WHERE receipt_code = ?', [req.params.code], (err, rows) => {
+    //     //     if (err) {
+    //     //         res.status(400).json({"error":err.message});
+    //     //         return;
+    //     //     }
+    //     //     res.json({
+    //     //         "message": "success",
+    //     //         "data": rows
+    //     //     });
+    //     //     return;
+    //     // });
+
+    //    await run(req.params.code, req.params.time)
+    //         .then((response) => {
+    //             res.send(response[0]);
+    //             // console.log(response);
+    //             // var data = response[0];
+    //             // if (data) {
+    //             //     // db.run(`INSERT INTO receipts (receipt_code,company_name,po_box,mobile,tin,vrn,serial_no,uin,tax_office,customer_name,customer_id_type,customer_id, customer_mobile,receipt_no,z_number,receipt_date,receipt_time,total_excl_of_tax,total_tax,total_incl_of_tax) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+    //             //     //     [req.params.code,data.companyName, data.poBox, data.mobile, data.tin, data.vrn, data.serialNo, data.uin, data.taxOffice, data.customerName, data.customerIdType, data.customerId, data.customerMobile, data.receiptNo, data.zNumber, data.receiptDate, data.receiptTime, data.totalExclOfTax, data.totalTax, data.totalInclOfTax]
+    //             //     // );
+    //             //     res.send(data);
+    //             //     return;
+    //             // }
+
+    //             // res.send({status: true});
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         });
+    // } catch {
+    //     res.json({});
+    // }
 });
 
 app.use((req, res) => {
--- tra-scraper.js
+++ tra-scraper.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-function run (code, time) {
+function run(code, time) {
     return new Promise(async (resolve, reject) => {
         try {
 
@@ -20,16 +20,16 @@ function run (code, time) {
             if (page.url() == `https://verify.tra.go.tz/Verify/Verified?Secret=${timeStr}`) {
                 let contents = await page.evaluate(() => {
                     let results = [];
-    
+
                     // invoice header section
                     const invoiceHeader = document.querySelectorAll(".invoice-header b");
-    
+
                     // invoice info section
                     const invoiceInfo = document.querySelectorAll(".invoice-info .invoice-col b");
-    
+
                     // invoice tables
                     const invoiceTable = document.querySelectorAll('.table table.table tbody');
-    
+
                     // company
                     const companyName = invoiceHeader[0].innerText.trim();
                     const poBox = invoiceInfo[0].innerText.trim();
@@ -39,53 +39,55 @@ function run (code, time) {
                     const serialNo = invoiceInfo[4].nextSibling.textContent.trim();
                     const uin = invoiceInfo[5].nextSibling.textContent.trim();
                     const taxOffice = invoiceInfo[6].nextSibling.textContent.trim();
-    
+
                     // customer
                     const customerName = invoiceHeader[1].nextSibling.textContent.trim();
                     const customerIdType = invoiceHeader[2].nextSibling.textContent.trim();
                     const customerId = invoiceHeader[3].nextSibling.textContent.trim();
                     const customerMobile = invoiceHeader[4].nextSibling.textContent.trim();
-    
+
                     // receipt
-                    const receiptNo = invoiceHeader[5].nextSibling.textContent.trim();
+                    const receiptNumber = invoiceHeader[5].nextSibling.textContent.trim();
                     const zNumber = invoiceHeader[6].nextSibling.textContent.trim();
                     const receiptDate = invoiceHeader[7].nextSibling.textContent.trim();
                     const receiptTime = invoiceHeader[8].nextSibling.textContent.trim();
-    
+                    const receiptVerificationCode = '';
+
                     // prices
-                    const totalExclOfTax = invoiceTable[1].children[0].children[1].innerText.trim();
-                    const totalTax = invoiceTable[1].children[1].children[1].innerText.trim();
-                    const totalInclOfTax = invoiceTable[1].children[2].children[1].innerText.trim();
-                    
+                    const receiptTotalExclOfTax = invoiceTable[1].children[0].children[1].innerText.trim();
+                    const receiptTotalTax = invoiceTable[1].children[1].children[1].innerText.trim();
+                    const receiptTotalInclOfTax = invoiceTable[1].children[2].children[1].innerText.trim();
+
                     results.push({
-                        companyName,
-                        poBox,
-                        mobile,
-                        tin,
-                        vrn,
-                        serialNo,
-                        uin,
-                        taxOffice,
-                        customerName,
-                        customerIdType,
-                        customerId,
-                        customerMobile,
-                        receiptNo,
-                        zNumber,
-                        receiptDate,
-                        receiptTime,
-                        totalExclOfTax,
-                        totalTax,
-                        totalInclOfTax,
+                        'company_name': companyName,
+                        'p_o_box': poBox,
+                        'mobile': mobile,
+                        'tin': tin,
+                        'vrn': vrn,
+                        'serial_no': serialNo,
+                        'uin': uin,
+                        'tax_office': taxOffice,
+                        'customer_name': customerName,
+                        'customer_id_type': customerIdType,
+                        'customer_id': customerId,
+                        'customer_mobile': customerMobile,
+                        'receipt_number': receiptNumber,
+                        'receipt_z_number': zNumber,
+                        'receipt_date': receiptDate,
+                        'receipt_time': receiptTime,
+                        'receipt_verification_code': receiptVerificationCode,
+                        'receipt_total_excl_of_tax': receiptTotalExclOfTax,
+                        'receipt_total_tax': receiptTotalTax,
+                        'receipt_total_incl_of_tax': receiptTotalInclOfTax,
                     });
-    
+
                     return results;
                 });
                 resolve(contents);
             } else {
                 resolve([]);
             }
-            
+
             browser.close();
         } catch (e) {
             reject(e);
